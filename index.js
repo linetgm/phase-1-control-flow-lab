f9714bffc64ad6e0bef0a008109b0406a6b8d41f
Linet <muthiigathoni@gmail.com>
2023-06-19 00:30:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greeting
+  if (feet<=400){
+    greeting = "This one is on me!";
+  }
+  else if (feet>2000 && feet<2500){
+    greeting = "I will gladly take your thirty bucks.";
+  }
+  else if (feet>2500){
+    greeting = "No can do."
+  }
+  return greeting;
 }
+scuberGreetingForFeet(2501);
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const greeting = city == "NYC" ? "Ok, sounds good." : "No go.";
+  return greeting;
 }
+ternaryCheckCity("Pittsburgh");
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let greeting;
+  if (tip == "generous"){
+    greeting = "Thank you so much."
+  }
+  else if (tip == "not as generous"){
+    greeting = "Thank you."
+  }
+  else{
+    greeting = "Bye."
+  }
+  return greeting
 }
+switchOnCharmFromTip();
